9859430e41508ca8435b2332cdcc87abe29e6049
lewelyn7 <progit8itakademia>
2020-01-26 11:29:24 +0100

    all for 110 update

--- projekt.py
+++ projekt.py
@@ -95,7 +95,6 @@ def gauss(A):
     n = len(A)
 
     for i in range(0, n):
-        # Search for maximum in this column
         maxEl = abs(A[i][i])
         maxRow = i
         for k in range(i+1, n):
@@ -103,13 +102,11 @@ def gauss(A):
                 maxEl = abs(A[k][i])
                 maxRow = k
 
-        # Swap maximum row with current row (column by column)
         for k in range(i, n+1):
             tmp = A[maxRow][k]
             A[maxRow][k] = A[i][k]
             A[i][k] = tmp
 
-        # Make all rows below this one 0 in current column
         for k in range(i+1, n):
             c = -A[k][i]/A[i][i]
             for j in range(i, n+1):
@@ -118,7 +115,6 @@ def gauss(A):
                 else:
                     A[k][j] += c * A[i][j]
 
-    # Solve equation Ax=b for an upper triangular matrix A
     x = [0 for i in range(n)]
     for i in range(n-1, -1, -1):
         x[i] = A[i][n]/A[i][i]
